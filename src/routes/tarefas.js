504dc94bf7eb5e14ceb14939db8c0f422b465ef6
Micaela Alves <micaela.alves93@gmail.com>
2021-11-04 23:11:29 -0300

    feat - rota para o update de tarefas

--- src/routes/tarefas.js
+++ src/routes/tarefas.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const { Tarefas } = require('../controllers');
 
 router
-  .post('/', Tarefas.create);
+  .post('/', Tarefas.create)
+  .put('/:id', Tarefas.update);
 
 module.exports = router;
