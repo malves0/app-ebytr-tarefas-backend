32f30963d69b0515442fefebeeab5e4e3a214879
Micaela Alves <micaela.alves93@gmail.com>
2021-11-04 22:22:51 -0300

    fix - adicionar campos a query

--- src/models/tarefas.js
+++ src/models/tarefas.js
@@ -1,8 +1,11 @@
 const connection = require('./connection');
 
 const create = ({ nome, listaId }) => connection()
-    .then((db) => db.collection('tarefas').insertOne({ nome, listaId}))
-    .then((result) => ({ tarefa: { nome, listaId, _id: result.insertedId}}));
+    .then((db) => 
+      db
+        .collection('tarefas')
+        .insertOne({ nome, listaId, status: 'pendente', dataDeCriacao: new Date() }))
+    .then((result) => (result));
 
 module.exports = {
   create,
