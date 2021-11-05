9837d68f8ee31e27c42f642ccd186133e7f85a29
Micaela Alves <micaela.alves93@gmail.com>
2021-11-04 23:12:14 -0300

    feat - controller para o services de tarefas

--- src/services/tarefas.js
+++ src/services/tarefas.js
@@ -2,6 +2,9 @@ const { Tarefas } = require('../models');
 
 const create = (data) => Tarefas.create(data).then((res) => res);
 
+const update = (data) => Tarefas.update(data).then((res) => res);
+
 module.exports = {
   create,
+  update,
 };
