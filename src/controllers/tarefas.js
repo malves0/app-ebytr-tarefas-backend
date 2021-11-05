77a46977d9aa0c7d67f8d4554369dfa897dde237
Micaela Alves <micaela.alves93@gmail.com>
2021-11-04 23:11:56 -0300

    feat - controller para o update de tarefas

--- src/controllers/tarefas.js
+++ src/controllers/tarefas.js
@@ -1,13 +1,22 @@
 const { Tarefas } = require('../services');
-const { SUCCESS_OK } = require('../utils/statusCode');
+const { SUCCESS_OK, SUCCESS_CREATED } = require('../utils/statusCode');
 
 const create = (req, res, next) => {
   const { nome, listaId } = req.body;
   Tarefas.create({ nome, listaId})
+    .then((tarefa) => res.status(SUCCESS_CREATED).json(tarefa))
+    .catch((err) => next(err));
+};
+
+const update = (req, res, next) => {
+  const { id } = req.params;
+  const { nome, status } = req.body;
+  Tarefas.update({ id, nome, status })
     .then((tarefa) => res.status(SUCCESS_OK).json(tarefa))
     .catch((err) => next(err));
 };
 
 module.exports ={
   create,
+  update
 }
